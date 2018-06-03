8dae8ba0df2b07506b7699b0280790cb71a61a03
Dale Nacianceno <dale.nash01@gmail.com>
2018-06-03 09:01:45 -0400

    Variable Declaration

--- src/Player2.java
+++ src/Player2.java
@@ -1,3 +1,15 @@
+import javafx.scene.image.*;
+import javafx.geometry.Rectangle2D;
+
 public class Player2 {
+
+    private Image image;        // Image
+    private double x;           // X Position
+    private double y;           // Y Position
+    private double velx;        // X Velocity
+    private double vely;        // Y Velocity
+    private double width;       // Width
+    private double height;      // Height
+    private Rectangle2D hitbox; // Hitbox
 
 }
